fa16a918faf44b4109c2c133a8bb68f49bb6207d
jiangxingqi <xingqijiang@sohu-inc.com>
2018-07-14 12:05:09 +0800

    txt2csv 时间过滤

--- txt2csv_call.py
+++ txt2csv_call.py
@@ -7,14 +7,13 @@ Created on Thu Apr 12 09:47:10 2018
 import cueb
 
 #category_file_path = 'source/variety.txt'
-#txt_path = 'data-text
+#txt_path = 'data-text'
 
 category_file_path = '/Sourcedata/Codetable/variety.txt'
-
 txt_path = '/Sourcedata/Future/Day'
 
 rs_csv_path = 'data-csv'
 
-cueb.txt2csv.txt2csv_function(category_file_path, txt_path, rs_csv_path)
+cueb.txt2csv.txt2csv_function(category_file_path, txt_path, rs_csv_path, "2018/6/1")
 
 #cueb.txt2csv.txt2csv_function(category_file_path,'data-text','data-csv')
